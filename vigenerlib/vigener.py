af68e211e6c8886ca66757cb8513bb0f58276f68
OndrejZetrim <ondra.zema@seznam.cz>
2022-11-09 22:45:01 +0100

    search secret

--- vigenerlib/vigener.py
+++ vigenerlib/vigener.py
@@ -71,6 +71,11 @@ class Vigener:
         self._cryptogram_table: list[list[str]] = []
         for i in range(self._key_length):
             self._cryptogram_table.append([])
+        self._vigener_table: list[list[str]] = []
+        for i in range(26):
+            self._vigener_table.append([])
+            for j in range(26):
+                self._vigener_table[i].append(chr(((i + j) % 26) + 65))
 
     def load_cryptogram(self, cryptogram: str) -> None:
         self._cryptogram = cryptogram
@@ -78,10 +83,11 @@ class Vigener:
             self._cryptogram_table[index % self._key_length + 1].append(i)
     
     def load_language(self, analysis: dict[str, float]) -> None:
-        self._language_analysis = analysis
+        # self._language_analysis = analysis
+        self._language_analysis = {k: v for k, v, in reversed(sorted(analysis.items(), lambda item: item[1]))}
 
-    def analyze_cryptogram(self):
-        secret: list[str] = []
+    def analyze_cryptogram(self) -> None:
+        frequency: list[str] = []
         for col in self._cryptogram_table:
             
             analyze: dict[str, int] = {}
@@ -98,9 +104,24 @@ class Vigener:
                 if i[1] > max_value:
                     max_value = i[1]
                     max_character = i[0]
-            
-            #najit v vigenerove tabulce v jake radce je "e" pro toto max_character 
-        
+            frequency.append(max_character)
+
+        for i in self._language_analysis.items():
+            secret_key = self.search_key(self, frequency, i[1])
+            #decode a otestování se slovníkem
+    def decode(self, secret_key: str)->str:
+        pass
+
+    def search_key(self, frequency: list[str], frequent_char: str) -> str:
+        secret_key: list[str] = []
+        for i in frequency:
+            for j in self._vigener_table:
+                if j[ord(frequent_char) - ord("A")] == i:
+                    secret_key.append(j[0])
+                    break
+        return "".join(secret_key)
+
+
 
     
     def decode(self):
